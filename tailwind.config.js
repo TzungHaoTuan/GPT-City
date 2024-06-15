4247fd530e3804ac747a10f77c4015187527c7af
Elliott Tuan <gbs.tuan@gmail.com>
2024-06-15 13:59:03 +0800

    Add pages and customize daisy theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,5 +14,19 @@ module.exports = {
       },
     },
   },
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["dark"],
+          primary: "#3d899d",
+          secondary: "#2f4e5b",
+          accent: "#bcdee5",
+          neutral: "#182a34",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
