238a3d5b885fbc09a57bcd6f3aef3545128a6081
Elliott Tuan <gbs.tuan@gmail.com>
2024-08-19 21:50:34 +0800

    adjust dark themes color and landing page style

--- app/page.js
+++ app/page.js
@@ -1,19 +1,23 @@
 import Link from "next/link";
+import { SiOpenaigym } from "react-icons/si";
 
 export default function Home() {
   return (
-    <div className="hero min-h-screen bg-neutral text-white">
-      <div className="hero-content flex-col max-w-md">
-        <h1 className="text-6xl font-bold">CityGPT</h1>
-        <div>
-          <p className="text-center text-lg leading-relaxed py-6">
-            Welcome to CityGPT, your ultimate source for comprehensive and
-            up-to-date city information powered by artificial intelligence.
-          </p>
+    <div className="bg-primary bg-opacity-25">
+      <div className="hero min-h-screen bg-gradient-to-br from-neutral text-white">
+        <SiOpenaigym className="fixed top-12 left-16 w-12 h-12 text-accent hover:animate-spin" />
+        <div className="hero-content flex-col max-w-[500px]">
+          <h1 className="text-8xl font-bold text-primary">CityGPT</h1>
+          <div>
+            <p className="text-center text-xl leading-relaxed py-6">
+              Welcome to CityGPT, your ultimate source for comprehensive and
+              up-to-date city information powered by artificial intelligence.
+            </p>
+          </div>
+          <Link href="/chat" className="btn btn-secondary border border-accent">
+            Get Started
+          </Link>
         </div>
-        <Link href="/chat" className="btn btn-primary">
-          Get Started
-        </Link>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,37 +7,37 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      //   backgroundImage: {
+      //     "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      //     "gradient-conic":
+      //       "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      //   },
     },
   },
   daisyui: {
     themes: [
       "light",
       "dark",
-      // {
-      //   light: {
-      //     ...require("daisyui/src/theming/themes")["light"],
-      //     primary: "#3d899d",
-      //     secondary: "#2f4e5b",
-      //     accent: "#bcdee5",
-      //     neutral: "#182a34",
-      //     "base-100": "#ffffff",
-      //   },
-      // },
-      // {
-      //   dark: {
-      //     ...require("daisyui/src/theming/themes")["dark"],
-      //     primary: "#3d899d",
-      //     secondary: "#2f4e5b",
-      //     accent: "#bcdee5",
-      //     neutral: "#182a34",
-      //     "base-100": "#000000",
-      //   },
-      // },
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          primary: "#3d899d",
+          secondary: "#2f4e5b",
+          accent: "#bcdee5",
+          neutral: "#182a34",
+          "base-100": "#ffffff",
+        },
+      },
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["dark"],
+          primary: "#D3FFD1",
+          secondary: "#2A4728",
+          accent: "#6BFF66",
+          neutral: "#181D1F",
+          "base-100": "#000000",
+        },
+      },
     ],
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
